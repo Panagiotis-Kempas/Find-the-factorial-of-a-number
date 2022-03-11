395b557629ff40c4c15d7bfd316a8e73b6fdf173
Panos1996 <panos2171996@gmail.com>
2022-03-11 10:40:18 +0200

    Add project files.

---
+++ Exercises/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32( Console.ReadLine());
+            double factorial = Factorial(number);
+            Console.WriteLine("Factorial of " + number.ToString() + "=" + factorial.ToString());
+        }
+
+        public static double Factorial(int number)
+        {
+            if(number == 0)
+                return 1;
+
+            return number * Factorial(number - 1);          
+        }
+
+
+
+    }
+}
